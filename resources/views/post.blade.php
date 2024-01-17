58ffe78b4a617d10287ea774ea49722a987b0fda
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-17 19:45:13 +0330

    Implement a basic html posts blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css" />
+    <title>Posts</title>
+</head>
+
+<body>
+    <article>
+        <h2>
+            <a href="/post">
+                My first post
+            </a>
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, earum eaque ducimus corporis eos magni velit error culpa odit, praesentium in excepturi
+            dolor aperiam nostrum. Laudantium unde reiciendis doloribus dolore!
+        </p>
+    </article>
+    <a href="/">Back to home</a>
+</body>
+
+</html>
